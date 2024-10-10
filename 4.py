b56d3316287edca899007e9dd527a3e9337abb41
zetik27 <sedovkirill541@gmail.com>
2024-10-10 16:00:51 +0000

    5/6

---
+++ 4.py
@@ -0,0 +1,7 @@
+a=int(input())
+q=int(input())
+n=int(input())
+print(a)
+for i in (1,n,1):
+    a=a*q
+    print(a)
---
+++ 7.py
@@ -0,0 +1,7 @@
+a =int(input())
+b = int(input())
+if (a%b) ==0:
+    print('Yes')
+else:
+    print('No')
+    print(a%b)
