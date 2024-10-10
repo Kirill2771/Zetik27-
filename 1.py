d435e9c66360665c5e08db35182ea2c069e418ff
zetik27 <sedovkirill541@gmail.com>
2024-10-10 14:59:28 +0000

    Конец лекции

---
+++ 1.py
@@ -0,0 +1,28 @@
+'''
+a = 3 
+if a > 5:
+    print(f'hello 5')
+elif a < 2:
+    print('hello 2')
+else:
+    print(f'Tupo hello')    
+
+    
+    '''
+'''
+a = 3 
+b = 4
+c = 5
+if a > 4 and b == 2:
+    print("Good")
+elif b > 3 or c ==5
+
+'''
+'''
+for i in 1,3,4:
+    print(i**2, end=" ")
+
+for i in 1,3,4:
+    print(i**2, sep="-")
+
+    '''
